630f447aea44a43027fa9a40a8ee2017f8ae4693
Narendra G O <go.narendra@outlook.com>
2024-01-13 11:56:30 +0530

    Initial Setup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, APIRouter
 from fastapi.middleware.cors import CORSMiddleware
 from routers import notes
+import uvicorn
 
 
 app=FastAPI(swagger_ui_parameters={"syntaxHighlight":True})
@@ -10,4 +11,14 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(notes)
+app.include_router(notes.router)
+
+@app.get("/")
+async def home():
+    """
+        This is the home route.
+    """
+    return ["Welcome to Notes App!"]
+
+if __name__=="__main__":
+    uvicorn.run(app=app,host="0.0.0.0",port=8800)
